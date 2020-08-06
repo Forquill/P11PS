01e564b66fae3486614e657b0aaff65dfaff1b42
18050268 <mikunation01>
2020-08-06 13:15:31 +0800

    Initial commit + No Enhancements Yet

---
+++ app/src/main/java/sg/edu/rp/c346/p11ps/BioFragment.java
@@ -0,0 +1,88 @@
+package sg.edu.rp.c346.p11ps;
+
+import android.app.AlertDialog;
+import android.content.DialogInterface;
+import android.os.Bundle;
+
+import androidx.appcompat.app.ActionBarDrawerToggle;
+import androidx.core.view.GravityCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class BioFragment extends Fragment {
+
+    public BioFragment() {
+        // Required empty public constructor
+    }
+
+    private DrawerLayout drawerLayout;
+    ActionBarDrawerToggle drawerToggle;
+
+    TextView tvInfo;
+    Button btnEdit;
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        View view = inflater.inflate(R.layout.biofragment, container, false);
+
+        tvInfo = view.findViewById(R.id.tvInfo);
+        btnEdit = view.findViewById(R.id.btnEdit);
+        drawerLayout = view.findViewById(R.id.drawer_layout);
+
+        btnEdit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                AlertDialog.Builder alertDialog = new AlertDialog.Builder(getActivity());
+                alertDialog.setTitle("Edit Bio");
+                final EditText input = new EditText(getActivity());
+
+                alertDialog.setView(input);
+
+                alertDialog.setPositiveButton("YES",
+                        new DialogInterface.OnClickListener() {
+                            public void onClick(DialogInterface dialog, int which) {
+                                tvInfo.setText(input.getText().toString());
+                            }
+                        });
+                alertDialog.setNegativeButton("NO",
+                        new DialogInterface.OnClickListener() {
+                            public void onClick(DialogInterface dialog, int which) {
+                                dialog.cancel();
+                            }
+                        });
+
+                alertDialog.show();
+            }
+        });
+
+        FloatingActionButton fab = view.findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (!drawerLayout.isDrawerOpen(GravityCompat.START)){
+                    drawerLayout.openDrawer(GravityCompat.START);
+                } else{
+                    drawerLayout.closeDrawer(GravityCompat.END);
+                }
+            }
+        });
+
+        return view;
+    }
+}
